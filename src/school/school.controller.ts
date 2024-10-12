3366a1025758aee38779f8d5086e93d44bab17be
JuanUT <jdcruzt@ut.edu.co>
2024-10-12 03:04:50 -0500

    fix: :bug: Consultas a coleccion users

--- src/school/school.controller.ts
+++ src/school/school.controller.ts
@@ -1,14 +1,15 @@
 import { Controller, Get, Put, Body, Param } from '@nestjs/common';
 import { SchoolService } from './school.service';
-import { ISchool } from './interfaces/school.interface';
+// import { ISchool } from './interfaces/school.interface';
 import { UpdateSchoolDto } from './dto/update-school.dto';
+import { IUser } from 'src/user/interfaces/user.interface';
 
 @Controller('school')
 export class SchoolController {
   constructor(private readonly schoolService: SchoolService) {}
 
   @Get(':id')
-  async getSchoolProfile(@Param('id') id: string): Promise<ISchool> {
+  async getSchoolProfile(@Param('id') id: string): Promise<IUser> {
     return await this.schoolService.getSchoolProfile(id);
   }
 
@@ -16,7 +17,7 @@ export class SchoolController {
   async updateSchoolProfile(
     @Param('id') id: string,
     @Body() school: UpdateSchoolDto,
-  ): Promise<ISchool> {
+  ): Promise<IUser> {
     return await this.schoolService.updateSchoolProfile(id, school);
   }
 }
--- src/school/school.service.ts
+++ src/school/school.service.ts
@@ -1,17 +1,18 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { ISchool } from './interfaces/school.interface';
+// import { ISchool } from './interfaces/school.interface';
 import { UpdateSchoolDto } from './dto/update-school.dto';
 import { School } from './model/school.schema';
+import { IUser } from 'src/user/interfaces/user.interface';
 
 @Injectable()
 export class SchoolService {
   constructor(
-    @InjectModel(School.name) private readonly schoolModel: Model<ISchool>,
+    @InjectModel(School.name) private readonly schoolModel: Model<IUser>,
   ) {}
 
-  async getSchoolProfile(id: string): Promise<ISchool> {
+  async getSchoolProfile(id: string): Promise<IUser> {
     const schoolProfile = await this.schoolModel.findById(id).exec();
     if (!schoolProfile) {
       throw new NotFoundException(`Colegio con ID ${id} no encontrado`);
@@ -22,7 +23,7 @@ export class SchoolService {
   async updateSchoolProfile(
     id: string,
     updateSchoolDto: UpdateSchoolDto,
-  ): Promise<ISchool> {
+  ): Promise<IUser> {
     const updatedSchoolProfile = await this.schoolModel
       .findByIdAndUpdate(id, updateSchoolDto, {
         new: true,
